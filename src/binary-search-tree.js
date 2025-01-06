c80e086073430f996c3683302f421cf6baa09169
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-01-07 02:18:09 +0300

    init: start  task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,109 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.rooT = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rooT;
+  }
+  add(data) {
+    this.rooT = addNode(this.rooT, data);
+    function addNode(node, data) {
+      if (!node) return new Node(data);
+      if (!node.data === data) return node;
+      if (data < node.data) {
+        node.left = addNode(node.left, data);
+      } else {
+        node.right = addNode(node.right, data);
+      }
+      return node;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchNode(this.rooT, data);
+    function searchNode(node, data) {
+      if (!node) return false;
+      if (node.data === data) return true;
+      if (data < node.data) {
+        return searchNode(node.left, data);
+      } else {
+        return searchNode(node.right, data);
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let correnNode = this.rooT;
+    while (correnNode) {
+      if (data === correnNode.data) {
+        return correnNode;
+      }
+      if (data < correnNode.data) {
+        correnNode = correnNode.left;
+      } else {
+        correnNode = correnNode.right;
+      }
+    }
+    return null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rooT = removeNode(this.rooT, data);
+    function removeNode(node, data) {
+      if (!node) return false;
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) return null;
+        if (!node.left) {
+          return (node = node.right);
+        }
+        if (!node.right) {
+          return (node = node.left);
+        }
+        let oneNode = node.right;
+        while (oneNode.left) {
+          oneNode = oneNode.left;
+        }
+        node.data = oneNode.data;
+        node.right = removeNode(node.right, oneNode.data);
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rooT) return;
+    let correnNode = this.rooT;
+    while (correnNode.left) {
+      correnNode = correnNode.left;
+    }
+    return correnNode.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let correnNode = test.rooT;
+    if (!correnNode) return;
+    while (correnNode.right) {
+      correnNode = correnNode.rightl;
+    }
+    return correnNode.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.stack = [];
+  }
+  push(element) {
+    return this.stack[this.stack.length] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let onPop = this.stack[this.stack.length -1];
+    this.stack.splice(-1, 1);
+    return onPop;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length -1];
   }
 }
 
